d57f335c87fff9ef0a7080072f36e87ccb505087
Nguyen Van Dai <nguyenvandaibe@gmail.com>
2023-10-14 22:09:53 +0700

    Thiết lập đăng nhập bằng username thay vì email

---
+++ database/migrations/2023_10_14_141727_add_username_column_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username', 255)->after('email_verified_at')->unique()->comment('Ten dang nhap');
+            $table->string('avatar')->after('remember_token')->nullable()->comment('URL anh dai dien');
+            $table->softDeletes(); // Bo sung soft delete cho table users
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+        });
+    }
+};
